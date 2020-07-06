79494be2b8c09e44ac879d19c6e57eb953c3347c
tesla <rensimiao.ben@gmail.com>
2020-07-06 15:59:44 -0400

    Uploading to DCC to do ablation study regarding the Pre-train

--- parameters.py
+++ parameters.py
@@ -9,16 +9,16 @@ IMG_L = 512
 IMG_W = 512
 NUM_WORKERS = 2
 CUT_SQUARE = False
-PRETRAIN = True
+PRETRAIN = False
 # Mac set
 #ROOT_DIR = '/Users/ben/Downloads/Eye segmentation project/OCT_bscans_raw/small_set10'
 #LABEL_FILE = '/Users/ben/Downloads/Eye segmentation project/OCT_bscans_raw/small_set10/label_file.csv'
 
 # Small set on Tesla
-TRAIN_ROOT_DIR = os.path.join('dataIn', 'train')
-TRAIN_LABEL_FILE = os.path.join('dataIn', 'train', 'label_file.csv')
-TEST_ROOT_DIR = "D:\AML\Eyeseg_data\small_set10\test" #os.path.join('dataIn',  'test_small')
-TEST_LABEL_FILE = "D:\AML\Eyeseg_data\small_set10\test\label_file.csv" #os.path.join('dataIn', 'test_small','label_file.csv')
+#TRAIN_ROOT_DIR = os.path.join('dataIn', 'train')
+#TRAIN_LABEL_FILE = os.path.join('dataIn', 'train', 'label_file.csv')
+#TEST_ROOT_DIR = "D:\AML\Eyeseg_data\small_set10\test" #os.path.join('dataIn',  'test_small')
+#TEST_LABEL_FILE = "D:\AML\Eyeseg_data\small_set10\test\label_file.csv" #os.path.join('dataIn', 'test_small','label_file.csv')
 
 # Full set on Tesla
 # TRAIN_ROOT_DIR = os.path.join('dataIn', 'small_set10', 'train')
@@ -33,10 +33,10 @@ TEST_LABEL_FILE = "D:\AML\Eyeseg_data\small_set10\test\label_file.csv" #os.path.
 #TEST_LABEL_FILE = '/work/sr365/OCT_bscans_raw/small_set10/test/label_file.csv'
 
 # Big set
-#TRAIN_ROOT_DIR = '/work/sr365/OCT_bscans_raw/train'
-#TRAIN_LABEL_FILE = '/work/sr365/OCT_bscans_raw/train/label_file.csv'
-#TEST_ROOT_DIR = '/work/sr365/OCT_bscans_raw/test'
-#TEST_LABEL_FILE = '/work/sr365/OCT_bscans_raw/test/label_file.csv'
+TRAIN_ROOT_DIR = '/work/sr365/OCT_bscans_raw/train'
+TRAIN_LABEL_FILE = '/work/sr365/OCT_bscans_raw/train/label_file.csv'
+TEST_ROOT_DIR = '/work/sr365/OCT_bscans_raw/test'
+TEST_LABEL_FILE = '/work/sr365/OCT_bscans_raw/test/label_file.csv'
 
 
 # Training related parameters
